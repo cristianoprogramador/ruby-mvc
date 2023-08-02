a1e78d3cd8df7f78cb3c087450edddc32a489e03
Cristiano Pereira da Silva <102186472+cristianoprogramador@users.noreply.github.com>
2023-08-02 15:09:04 -0300

    testando conexões entre bancos

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_02_140338) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_02_172946) do
+  create_table "account_histories", force: :cascade do |t|
+    t.date "date"
+    t.integer "accountsupply_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["accountsupply_id"], name: "index_account_histories_on_accountsupply_id"
+  end
+
   create_table "accounts", force: :cascade do |t|
     t.integer "author_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_accounts_on_author_id"
+  end
+
+  create_table "accountsupplies", force: :cascade do |t|
+    t.integer "number"
+    t.integer "supplier_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["supplier_id"], name: "index_accountsupplies_on_supplier_id"
   end
 
   create_table "appointments", force: :cascade do |t|
@@ -62,7 +78,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_02_140338) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "suppliers", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "account_histories", "accountsupplies"
   add_foreign_key "accounts", "authors"
+  add_foreign_key "accountsupplies", "suppliers"
   add_foreign_key "appointments", "doctors"
   add_foreign_key "appointments", "patients"
   add_foreign_key "books", "authors"
